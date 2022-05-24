014d103484fdc59fae9cf34bd7ef2a07340d9b0e
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-05-24 19:45:41 -0400

    feat: start adding features

---
+++ cmd/static-server/main.go
@@ -0,0 +1,21 @@
+package static_server
+
+// serve http
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Based Go Microservice")
+}
+func main() {
+    port := 8080
+    serveString := ":" + string(port)
+    http.HandleFunc("/", handler)
+
+    err := http.ListenAndServe(serveString, nil)
+    if err != nil {
+        panic(err)
+    }
+}
